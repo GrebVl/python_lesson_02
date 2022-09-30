9d51946857b11fe0a7d822f8bd2cbf20acfdb33e
Vladimir <chertonovskiy@icloud.com>
2022-09-30 20:34:55 +0700

    commit task_03.py

---
+++ task_03.py
@@ -0,0 +1,24 @@
+# Задание 3 Задайте список из n чисел последовательности
+# (1+1/n)^n и выведите на экран их сумму,
+# округлённую до трёх знаков после точки.
+#
+# Пример:
+#
+# Для n = 6 -> 14.072
+
+num_us = int(input('Введите число n: '))
+
+list_ = []
+for i in range(num_us + 1):
+    list_.append(i)
+    if i == 0:
+        list_[i] = 0
+    else:
+        list_[i] = (1 + 1/i) ** i
+
+summ_ = 0
+for j in range(num_us + 1):
+    summ_ += list_[j]
+
+
+print(num_us, '->>', round(summ_, 3))
